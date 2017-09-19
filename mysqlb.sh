aad5f74a88971ca76f970ff77f6d784e29f860af
Marcel Haazen <mhaazen@live.nl>
2017-09-19 15:59:26 +0200

    Update mysqlb.sh
    Added the date

--- mysqlb.sh
+++ mysqlb.sh
@@ -6,6 +6,8 @@
 # Other options
  backup_path="/backup/mysql"
  date=$(date +"%d-%b-%Y")
+#Make folder of the day
+ mkdir $backup_path/$date
 # Set default file permissions
  umask 177
 # List all databases on server
@@ -14,7 +16,7 @@ databases=`mysql --user=$user --password=$password --host=$host -e "SHOW DATABAS
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
-        mysqldump --user=$user --password=$password --host=$host --databases $db > $backup_path/$db-$date.sql
-        gzip $backup_path/$db-$date.sql
+        mysqldump --user=$user --password=$password --host=$host --databases $db > $backup_path/$date/$db-$date.sql
+        gzip $backup_path/$date/$db-$date.sql
     fi
 done
